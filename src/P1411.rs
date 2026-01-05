7c19a838264215af7b99ccd8517fb788665a24f6
thesnakefromthelemma <rafayaashary01@gmail.com>
2026-01-04 18:01:43 -0700

    Should've remembered Cayley-Hamilton

--- src/P1411.rs
+++ src/P1411.rs
@@ -1,5 +1,9 @@
 use std::cmp::Ordering::{Equal, Greater, Less};
 
+// Modulus from problem statement
+// It's critical that 7 * P * P < 2 ^ 63
+const P: i64 = i64::pow(10, 9) + 7;
+
 // 6 * [1, 1] [2, 2; 2, 3] ^ {n - 1} [1; 1]
 // (Coeffs computed w/ GHCi. Sadly the charpoly doesn't split mod p...)
 pub fn num_of_ways(n: i32) -> i32 {
@@ -7,52 +11,43 @@ pub fn num_of_ways(n: i32) -> i32 {
         Greater => 0,
         Equal => 12,
         Less => {
-            // Modulus from problem statement
-            // (must be 64, not 32, bits)
-            let p = u64::pow(10, 9) + 7;
-
             // Exponent
-            let mut pow = n - 1;
+            let mut exp = n - 1;
 
-            // Symmetric matrix being iteratively squared
-            let mut a_pow = 2;
-            let mut b_pow = 2;
-            let mut c_pow = 3;
+            // Iteratively squared matrix as lincomb of [1, 0; 0, 1] and [2, 2; 2, 3]
+            let mut a_pow = 0;
+            let mut b_pow = 1;
 
-            // Accumulated product
+            // Accumulated product matrix as lincomb of [1, 0; 0, 1] and [2, 2; 2, 3]
             let mut a_tot = 1;
             let mut b_tot = 0;
-            let mut c_tot = 1;
 
-            while 1 < pow {
-                if pow % 2 == 1 {
-                    let a_tot_new = (a_pow * a_tot + b_pow * b_tot) % p;
-                    let b_tot_new = (a_pow * b_tot + b_pow * c_tot) % p;
-                    let c_tot_new = (c_pow * c_tot + b_pow * b_tot) % p;
+            while 1 < exp {
+                if 1 & exp == 1 {
+                    // Multiply iteratively squared matrix and accumulator
+                    let a_tot_new = (a_pow * a_tot - 2 * b_pow * b_tot) % P;
+                    let b_tot_new = (a_pow * b_tot + b_pow * a_tot + 5 * b_pow * b_tot) % P;
                     a_tot = a_tot_new;
                     b_tot = b_tot_new;
-                    c_tot = c_tot_new;
                 }
 
-                let a_pow_new = (a_pow * a_pow + b_pow * b_pow) % p;
-                let b_pow_new = ((a_pow + c_pow) * b_pow) % p;
-                let c_pow_new = (c_pow * c_pow + b_pow * b_pow) % p;
+                // Square iteratively squared matrix
+                let a_pow_new = (a_pow * a_pow - 2 * b_pow * b_pow) % P;
+                let b_pow_new = ((2 * a_pow + 5 * b_pow) * b_pow) % P;
                 a_pow = a_pow_new;
                 b_pow = b_pow_new;
-                c_pow = c_pow_new;
 
-                pow >>= 1;
+                exp >>= 1;
             }
 
-            // It's now guaranteed that pow == 1
-            let a_tot_new = (a_pow * a_tot + b_pow * b_tot) % p;
-            let b_tot_new = (a_pow * b_tot + b_pow * c_tot) % p;
-            let c_tot_new = (c_pow * c_tot + b_pow * b_tot) % p;
+            // Now guaranteed that exp == 1
+            let a_tot_new = (a_pow * a_tot - 2 * b_pow * b_tot) % P;
+            let b_tot_new = (a_pow * b_tot + b_pow * a_tot + 5 * b_pow * b_tot) % P;
             a_tot = a_tot_new;
             b_tot = b_tot_new;
-            c_tot = c_tot_new;
 
-            ((6 * a_tot + 12 * b_tot + 6 * c_tot) % p) as i32
+            // Sign issues
+            ((12 * a_tot + 54 * b_tot + 66 * P) % P) as i32
         }
     }
 }
@@ -63,11 +58,11 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(num_of_ways(1), 12);
+        assert_eq!(num_of_ways(1), 12)
     }
 
     #[test]
     fn test2() {
-        assert_eq!(num_of_ways(5000), 30228214);
+        assert_eq!(num_of_ways(5000), 30228214)
     }
 }
--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Library file exposing eeach solution as a module
-// pub mod p0001;
-// pub mod p0003;
-// pub mod p0961;
-pub mod p1390;
-// pub mod p1411;
