d0086761c9d66a3dbffb9fa6490669265f90a49c
thesnakefromthelemma <rafayaashary01@gmail.com>
2026-01-08 10:36:21 -0700

    Solved P1458

--- src/P1458.rs
+++ src/P1458.rs
@@ -1,84 +1,50 @@
-use std::cmp::{min, max, Ordering::{Less, Equal}};
+use std::cmp::{max, min, Ordering::Less};
 
 const MAX_VAL: i32 = 1001;
 
-fn index_dp(len0: usize, len1: usize, mask: usize, ind0: usize, ind1: usize) -> usize {
-    let sum_of = |n| n * (n + 1) / 2;
-
-    let rank = ind0 + ind1;
-
-    let spill0 = rank - min(rank, len0);
-    let spill1 = rank - min(rank, len1 - 1);
-
-    (sum_of(ind0 + ind1) + ind0 - sum_of(spill0) - sum_of(spill1)) & mask
-}
-
+// Assumes 1 <= len0 <= len1
 pub fn max_dot_product(nums0: &Vec<i32>, nums1: &Vec<i32>) -> i32 {
+    // For convenience
     let len0 = nums0.len();
     let len1 = nums1.len();
-    // So we can assume len0 <= len1
-    if len0 > len1 {
-        return max_dot_product(nums1, nums0);
-    }
-    // Annoying case, evict it so that we can assume len0, len1 >= 2
-    if len0 < 2 || len1 < 2 {
-        return handle_empty(nums0, nums1);
-    }
 
-    // Compute minimal cache size that's power of 2 and calloc it
-    let cache_len = 1 << (2 * match len0.cmp(&len1) {
-        Equal => 2 * min(len0, len1) - 1,
-        _ => 2 * min(len0, len1),
-    } - 1).ilog2();
+    // Compute minimal ringbuffer cache size that's power of 2 and calloc it
+    let cache_len = 1 << (4 * len1 - 1).ilog2();
     let cache_mask = cache_len - 1;
     let mut cache: Vec<i32> = Vec::new();
     cache.resize(cache_len, 0);
 
-    // For convenience
-    let index = |ind0, ind1| index_dp(len0, len1, cache_mask, ind0, ind1);
+    let index = |i0, i1| (len1 * i0 + i1) & cache_mask;
 
-    // i0 == 0, i1 == 0
-    cache[index(0, 0)] = max(0, nums0[0] * nums1[0]);
+    // i0 == 0
+    {
+        // i1 == 0
+        cache[index(0, 0)] = max(0, nums0[0] * nums1[0]);
 
-    for rank in 1..len0 {
-        cache[index(0, rank)] = max(cache[index(0, rank - 1)], nums0[0] * nums1[rank]);
-
-        for i0 in 1..rank {
-            let i1 = rank - i0;
-            let ind = index(i0, i1);
-            cache[ind] = max(cache[index(i0 - 1, i1)], cache[index(i0, i1 - 1)]);
-            cache[ind] = max(cache[ind], cache[index(i0 - 1, i1 - 1)] + nums0[i0] * nums1[i1]);
-        }
-
-        cache[index(rank, 0)] = max(cache[index(rank - 1, 0)], nums0[rank] * nums1[0]);
-    }
-
-    for rank in len0..len1 {
-        cache[index(0, rank)] = max(cache[index(0, rank - 1)], nums0[0] * nums1[rank]);
-
-        for i0 in 1..len0 {
-            let i1 = rank - i0;
-            let ind = index(i0, i1);
-            cache[ind] = max(cache[index(i0 - 1, i1)], cache[index(i0, i1 - 1)]);
-            cache[ind] = max(cache[ind], cache[index(i0 - 1, i1 - 1)] + nums0[i0] * nums1[i1]);
+        // i1 != 0
+        for i1 in 1..len1 {
+            cache[index(0, i1)] = max(cache[index(0, i1 - 1)], nums0[0] * nums1[i1]);
         }
     }
 
-    for rank in len1..(len0 + len1 - 2) {
-        for i0 in (rank - len1 + 1)..len0 {
-            let i1 = rank - i0;
+    // i0 != 0
+    for i0 in 1..len0 {
+        // i1 == 0
+        cache[index(i0, 0)] = max(cache[index(i0 - 1, 0)], nums0[i0] * nums1[0]);
+
+        // i1 != 0
+        for i1 in 1..len1 {
             let ind = index(i0, i1);
             cache[ind] = max(cache[index(i0 - 1, i1)], cache[index(i0, i1 - 1)]);
-            cache[ind] = max(cache[ind], cache[index(i0 - 1, i1 - 1)] + nums0[i0] * nums1[i1]);
+            cache[ind] = max(
+                cache[ind],
+                cache[index(i0 - 1, i1 - 1)] + nums0[i0] * nums1[i1],
+            );
         }
     }
 
-    // We don't actually write cache[index(len0 - 0, len1 - 1)] to the cache,
-    // since we immediately access and consume it; instead we write to val
-    let mut val = max(cache[index(len0 - 2, len1 - 1)], cache[index(len0 - 1, len1 - 2)]);
-    val = max(val, cache[index(len0 - 2, len1 - 2)] + nums0[len0 - 1] * nums1[len1 - 1]);
-
-    // Check and handle bullshit case
+    // Check for and handle the bullshit case
+    let val = cache[index(len0 - 1, len1 - 1)];
     match 0.cmp(&val) {
         Less => val,
         _ => handle_empty(nums0, nums1),
@@ -97,7 +63,7 @@ fn handle_empty(nums0: &Vec<i32>, nums1: &Vec<i32>) -> i32 {
         min1 = min(min1, num.abs());
     }
 
-    - min0 * min1
+    -min0 * min1
 }
 
 #[cfg(test)]
@@ -106,16 +72,16 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(max_dot_product(&vec![2,1,-2,5], &vec![3,0,-6]), 18)
+        assert_eq!(max_dot_product(&vec![2, 1, -2, 5], &vec![3, 0, -6]), 18)
     }
 
     #[test]
     fn test2() {
-        assert_eq!(max_dot_product(&vec![3,-2], &vec![2,-6,7]), 21)
+        assert_eq!(max_dot_product(&vec![3, -2], &vec![2, -6, 7]), 21)
     }
 
     #[test]
     fn test3() {
-        assert_eq!(max_dot_product(&vec![-1,-1], &vec![1,1]), -1)
+        assert_eq!(max_dot_product(&vec![-1, -1], &vec![1, 1]), -1)
     }
 }
